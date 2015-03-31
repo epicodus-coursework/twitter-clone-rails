65261928e8b97b394d015bb923e06e577b3345f3
Lee Jorgensen and Matthew Terrill <meterrill@gmail.com>
2015-03-31 16:42:01 -0700

    add avatar to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :posts
-  
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,4 +13,5 @@ class User < ActiveRecord::Base
   def deliver_confirmation_instructions
     UserMailer.signup_confirmation(self).deliver_now
   end
+  
 end
